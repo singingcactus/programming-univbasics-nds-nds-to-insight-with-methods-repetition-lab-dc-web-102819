ffd5cc440cb0583c30b3ec8d0059272dc50317b7
Matt Bechtel <bechma02@gmail.com>
2019-10-21 18:29:45 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -25,10 +25,13 @@ def gross_for_director(d)
 end
 
 def list_of_directors(source)
+
   # Write this implementation
 end
 
 def total_gross(source)
+
+
   # Write this implementation
   #
   # Should use methods:
@@ -39,5 +42,3 @@ def total_gross(source)
   # returned by directors_totals, and add it to a running total. When done,
   # return the total
 end
-
-
